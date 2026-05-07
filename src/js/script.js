ea9dd1f1b78051090fe8d547f842fe383d4e8f60
Gustavo <ferreiratavaresg@gmail.com>
2026-05-07 09:38:34 -0300

    [Feat]Criando menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,18 @@ window.onload = function(){
         },1000)
     },3000)
 }
-
+// MENU HAMBURGUER
+document.addEventListener("DOMContentLoaded",()=>{
+        const menuIcone = document.getElementById("menu-icone");
+        const navMenu = document.querySelector("nav");
+ 
+        if(menuIcone && navMenu){
+            menuIcone.onclick=()=>{
+                navMenu.classList.toggle("active");
+                menuIcone.classList.toggle("open");
+            }
+        }
+})
 
 
 
